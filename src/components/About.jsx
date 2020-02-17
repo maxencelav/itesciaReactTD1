56231da7505a24c3d6a6443d160884d7c9b1cd19
Maxence Lavenu <maxence.lavenu@outlook.fr>
2020-02-17 15:29:01 +0100

    Updates to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ class About extends React.Component {
         <h3>L'équipe est constituée de :</h3>
         <ul>
             <li>Maxence Lavenu</li>
-            <li>Alyssia Prévoté</li>
+            <li>Alyssia Prévoté--Hauguel</li>
             <li>Jonathan Debailleux</li>
             <li>Juan Fernando Herrada</li>
         </ul>
