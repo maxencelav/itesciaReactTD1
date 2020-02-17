2a11e61924d2baf2190089b0ec6096d327bb5bbc
DogSobre <juanfernandoherrada13@gmail.com>
2020-02-17 15:01:43 +0100

    save

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import {
     Switch,
     Route,
     Link,
-    useParams
+    useParams,
+    useRouteMatch
 } from 'react-router-dom';
 
-export default function ParamsExample() {
+export default function NestingExample() {
   return(
       <Router>
         <div>
-
-          <h2>Account</h2>
-
             <ul>
                 <li>
                     <Link to="/accueil"> Accueil </Link>
@@ -30,21 +28,93 @@ export default function ParamsExample() {
                 </li>
             </ul>
 
-            <switch>
-                {/* eslint-disable-next-line react/jsx-no-undef */}
-                <Route path="/:id" children = {< Child />} />
-            </switch>
+            <hr/>
+
+            <Switch>
+                <Route exact path="/accueil">
+                    {/* eslint-disable-next-line react/jsx-no-undef */}
+                    <Accueil />
+                </Route>
+                <Route exact path = "/config">
+                    {/* eslint-disable-next-line react/jsx-no-undef */}
+                    <Config />
+                </Route>
+                <Route path="/a-propos">
+                    {/* eslint-disable-next-line react/jsx-no-undef */}
+                    <APropos />
+                </Route>
+            </Switch>
         </div>
       </Router>
   );
 }
 
-function Child(){
-    let { id } = useParams();
+function Accueil(){
     return(
-        <div>
-            <h3>ID: {id}</h3>
-        </div>
+      <div>
+          <h2>Acceuil</h2>
+      </div>
     );
 }
 
+function Config(){
+    return(
+      <div>
+          <h2>Configuration</h2>
+
+          <form>
+              <ul>
+                  <li>
+                      <label for="Nom">Nom : </label>
+                      <input type="text" name="Nom" id="Nom" placeholder="Tapie"/>
+                  </li>
+                  <li>
+                      <label for="Prenom">Prénom : </label>
+                      <input type="text" name="Prenom" id="Prenom" placeholder="Bernard"/>
+                  </li>
+              </ul>
+          </form>
+      </div>
+    );
+}
+
+function APropos(){
+    let { path , url } = useRouteMatch();
+    return(
+      <div>
+          <h2>À Propos</h2>
+
+          <ul>
+              <li>
+                  <Link to = {`${url}/first-info`}>Première info</Link>
+              </li>
+              <li>
+                  <Link to = {`${url}/second-info`}>Seconde info</Link>
+              </li>
+              <li>
+                  <Link to = {`${url}/third-info`}>Troisième info</Link>
+              </li>
+          </ul>
+
+          <Switch>
+              <Route exact path = {path}>
+                <h3>Choisissez une info</h3>
+              </Route>
+              <Route path = {`${path}/:aproposID`}>
+                  <APropos/>
+              </Route>
+          </Switch>
+
+      </div>
+    );
+}
+
+function aPropos() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    let {aproposID} = useParams();
+    return(
+      <div>
+          <h3>{aproposID}</h3>
+      </div>
+    );
+}
