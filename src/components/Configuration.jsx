c014ef39b8790d142ca78e2945d17ce83942ba69
DogSobre <juanfernandoherrada13@gmail.com>
2020-02-17 15:48:13 +0100

    update about names

--- src/components/Configuration.jsx
+++ src/components/Configuration.jsx
@@ -2,35 +2,24 @@ import React from 'react';
 
 class Configuration extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
         event.preventDefault();
+        this.props.name(event.target[0].value);
     }
 
     render() {
-    return(
-        <div>
-            <h2>Configuration</h2>
-            <form onSubmit={this.handleSubmit}>
-                <label>Nom :
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Envoyer" />
-            </form>
-        </div>);
-        }
+        return (
+            <div>
+                <h2>Configuration</h2>
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    <label>
+                        Nom :
+                        <input type="text" name="name"/>
+                    </label>
+                    <input type="submit" value="Envoyer"/>
+                </form>
+            </div>);
+    }
 }
 
 export default Configuration;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Configuration from './Configuration';
+
 
 class Home extends React.Component {
     render() {
-        return(
-            <div>
-                <h2>Accueil</h2>
-                <p>Hello</p>
-            </div>);
+    return(
+        <div>
+            <h2>Accueil</h2>
+            <p>Hello {this.props.name}</p>
+        </div>);
 
     }
 }
