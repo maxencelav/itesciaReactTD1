c4915799ec1e1a057a889b7c2c5e9c5f4a2796ad
DogSobre <juanfernandoherrada13@gmail.com>
2020-02-17 11:32:30 +0100

    basic from react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function Example() {
+  return(
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/accueil">Acceuil</Link>
+            </li>
+            <li>
+              <Link to="/config">Configuration</Link>
+            </li>
+            <li>
+              <Link to="/aPropos">À propos</Link>
+            </li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/accueil">
+              {/* eslint-disable-next-line react/jsx-no-undef */}
+              <Accueil />
+            </Route>
+            <Route path="/Config">
+              {/* eslint-disable-next-line react/jsx-no-undef */}
+              <Config />
+            </Route>
+            <Route path="/aPropos">
+              {/* eslint-disable-next-line react/jsx-no-undef */}
+              <APropos />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
-export default App;
+function Accueil() {
+    return (
+        <div>
+            <h2>Accueil</h2>
+        </div>
+    );
+}
+
+function Config() {
+    return (
+        <div>
+            <h2>Configuration</h2>
+        </div>
+    );
+}
+
+function APropos() {
+    return (
+        <div>
+            <h2>À Propos</h2>
+        </div>
+    );
+}
+
+
